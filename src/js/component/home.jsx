24486dcfab675342497e725db75c96ac7c6deb64
AgustinPungitore <agustinpungitore@gmail.com>
2022-08-27 03:04:39 +0000

    80% completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState ("")
+	const [lista, setLista] = useState ([])
+
+
+	function addTarea (e) {
+		
+		
+		if (e.key === "Enter"){
+			console.log(tarea)	
+			setLista([...lista,tarea])
+			setTarea("")
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<>
+		<div className="container col-8 text-center">	
+		<h1> My To-Do List </h1>
+			<div className="input-group mb-3">
+				<input type="text" className="form-control" placeholder="Agregar tarea" onChange={(e)=>setTarea(e.target.value)}
+			 onKeyDown={addTarea} value={tarea} />
+			</div>
 		</div>
-	);
+		<div className="container col-8 text-center">
+			<ul>
+			{lista.map ((item, index) => {return (<li  key={index}> {item} </li>)})}
+			</ul>
+		</div>
+	</>
+	
+		);
 };
 
 export default Home;
