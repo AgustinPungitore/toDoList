4297bc460c8444a223e0cf23a212006a3a14fedb
AgustinPungitore <agustinpungitore@gmail.com>
2022-09-03 19:37:44 +0000

    almost done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -20,18 +20,30 @@ const Home = () => {
 		}
 	}
 
+	
+	const eliminarTarea = (index) => {
+		console.log(index)
+		const listaNueva = lista.filter((item, i) => {
+			if (index !== i) {
+				return item
+			}
+		} )
+		setLista(listaNueva)
+
+	}
 	return (
 	<>
 		<div className="container col-8 text-center">	
 		<h1> My To-Do List </h1>
 			<div className="input-group mb-3">
 				<input type="text" className="form-control" placeholder="Agregar tarea" onChange={(e)=>setTarea(e.target.value)}
-			 onKeyDown={addTarea} value={tarea} />
+			 onKeyDown={addTarea} value={tarea} /> 
+			{/* value para setear un valor inicial */}
 			</div>
 		</div>
 		<div className="container col-8 text-center">
 			<ul>
-			{lista.map ((item, index) => {return (<li  key={index}> {item} </li>)})}
+			{lista.map ((item, index) => {return (<li  key={index}> {item} <button onClick={()=>eliminarTarea(index)}>🗑️</button></li>)})} 
 			</ul>
 		</div>
 	</>
